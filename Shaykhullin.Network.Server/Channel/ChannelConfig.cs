bead896a30c54604f963f17d0bc9584b564ee87f
Shaykhullin <yolorusher@gmail.com>
2018-03-31 08:53:34 +0300

    Channel Config

--- Shaykhullin.Network.Server/Channel/ChannelConfig.cs
+++ Shaykhullin.Network.Server/Channel/ChannelConfig.cs
@@ -1,11 +1,23 @@
-﻿namespace Shaykhullin.Network.Core
+﻿using System.Collections.Generic;
+
+namespace Shaykhullin.Network.Core
 {
 	internal class ChannelConfig : IChannelConfig
 	{
-		public Injectable Injectable { get; } = new Injectable();
+		private readonly IList<Dependency> dependencies;
+
+		public ChannelConfig(IList<Dependency> dependencies)
+		{
+			this.dependencies = dependencies;
+		}
 
 		public IRegisterBuilder<TRegister> Register<TRegister>()
-			where TRegister : class =>
-				Injectable.Register<TRegister>();
+			where TRegister : class
+		{
+			var dependency = new Dependency(typeof(TRegister));
+			dependencies.Add(dependency);
+
+			return new RegisterBuilder<TRegister>(dependency);
+		}
 	}
 }
--- Shaykhullin.Network.Server/Server/ServerConfig.cs
+++ Shaykhullin.Network.Server/Server/ServerConfig.cs
@@ -1,18 +1,42 @@
 ﻿using Shaykhullin.Network.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Shaykhullin.Network
 {
 	public sealed class ServerConfig : NodeConfig, IServerConfig
 	{
+		private readonly IDictionary<Type, ChannelDto> channels = new Dictionary<Type, ChannelDto>();
+
 		public IChannelConfig Channel<TChannel>() 
 			where TChannel : IChannel
 		{
-			return new ChannelConfig();
+			if(channels.TryGetValue(typeof(TChannel), out var dto))
+			{
+				return new ChannelConfig(dto.Dependencies);
+			}
+
+			dto = new ChannelDto(typeof(TChannel));
+			channels.Add(typeof(TChannel), dto);
+			return new ChannelConfig(dto.Dependencies);
 		}
 
 		public IServer Create(string host, int port)
 		{
 			return new Server();
+		}
+	}
+
+	public class ChannelDto
+	{
+		public Type Channel { get; }
+		public IList<Dependency> Dependencies { get; }
+
+		public ChannelDto(Type channel)
+		{
+			Channel = channel;
+			Dependencies = new List<Dependency>();
 		}
 	}
 }
