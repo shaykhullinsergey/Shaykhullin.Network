6437d9d526f630a0e9aa66f7b2c9c1f9e0c4490b
Shaykhullin <yolorusher@gmail.com>
2018-03-31 07:02:39 +0300

    NodeConfig

--- Shaykhullin.Network.Server/Server/ServerConfig.cs
+++ Shaykhullin.Network.Server/Server/ServerConfig.cs
@@ -2,20 +2,8 @@
 
 namespace Shaykhullin.Network
 {
-	public class ServerConfig : IServerConfig
+	public class ServerConfig : NodeConfig, IServerConfig
 	{
-		public IConfigBuilder<TEvent> When<TEvent>()
-			where TEvent : IHandlerEvent<object>
-		{
-			return new ConfigBuilder<TEvent>();
-		}
-
-		public IRegisterBuilder<TRegister> Register<TRegister>() 
-			where TRegister : class
-		{
-			return new RegisterBuilder<TRegister>();
-		}
-
 		public IChannelConfig Channel<TChannel>() 
 			where TChannel : IChannel
 		{
@@ -25,40 +13,6 @@ namespace Shaykhullin.Network
 		public IServer Create(string host, int port)
 		{
 			return new Server();
-		}
-
-		public ICompressionBuilder UseSerializer<TSerializer>()
-			where TSerializer : ISerializer
-		{
-			return new CompressionBuilder();
-		}
-
-		public IEncryptionBuilder UseCompression<TCompression>() 
-			where TCompression : ICompression
-		{
-			return new CompressionBuilder()
-				.UseCompression<TCompression>();
-		}
-
-		public ICommunicatorBuilder UseEncryption<TEncryption>() 
-			where TEncryption : IEncryption
-		{
-			return new EncryptionBuilder()
-				.UseEncryption<TEncryption>();
-		}
-
-		public IContainerBuilderBuilder UseCommunicator<TProtocol>() 
-			where TProtocol : ICommunicator
-		{
-			return new CommunicatorBuilder()
-				.UseCommunicator<TProtocol>();
-		}
-
-		public void UseContainer<TContainer>() 
-			where TContainer : IContainerBuilder
-		{
-			new ContainerBuilder()
-				.UseContainer<TContainer>();
 		}
 	}
 }
